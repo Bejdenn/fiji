821e5474cdbf5e30cfa0beafda98b15a090843aa
Dennis Bejze <53354166+Bejdenn@users.noreply.github.com>
2025-05-10 23:28:33 +0200

    Fix missing Python script

--- install/desktop/set-ghostty-default.sh
+++ install/desktop/set-ghostty-default.sh
@@ -2,4 +2,4 @@
 # Adding ghostty to nautilus contextual menu requires the python wrapper for the libraries
 sudo dnf install -y nautilus-python
 mkdir -p ~/.local/share/nautilus-python/extensions/
-cp open-ghostty.py ~/.local/share/nautilus-python/extensions/open-ghostty.py
+cp ~/.local/share/fiji/install/desktop/open-ghostty.py ~/.local/share/nautilus-python/extensions
