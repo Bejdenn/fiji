9ef527de40dfa7d968e6f147628d04a17db2c314
Dennis Bejze <53354166+Bejdenn@users.noreply.github.com>
2025-05-13 22:28:35 +0200

    Minor output adjustments

--- install/terminal/app-texlive.sh
+++ install/terminal/app-texlive.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Fiji wants to install TeX Live, a LaTeX installation and Zathura, a SyncTeX-compatible PDF viewer."
-gum confirm "Process with installation?" && sudo dnf install -y texlive-scheme-full zathura zathura-pdf-poppler || echo "skip installation of LaTeX"
+gum confirm "Process with installation?" && sudo dnf install -y texlive-scheme-full zathura zathura-pdf-poppler || echo "Skipped installation of LaTeX."
